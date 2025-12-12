aab6fd688b52777d56ef621acc6d8173d5dac7ba
Jae Kyun Shin <jkshin@yu.ac.kr>
2025-12-13 08:04:26 +0900

    loading.tsx 추가

---
+++ app/r/[...code]/loading.tsx
@@ -0,0 +1,11 @@
+export default function Loading() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#020617",
+        height: "100vh",
+        width: "100vw",
+      }}
+    />
+  );
+}
